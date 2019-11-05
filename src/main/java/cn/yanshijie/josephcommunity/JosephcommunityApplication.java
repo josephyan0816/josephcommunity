94c4e7fb2a97a89f00e7531b47dde1d4f104ca60
tom_glb <goodMorning@atguigu.com>
2019-11-05 17:31:26 +0800

    init-repo

---
+++ src/main/java/cn/yanshijie/josephcommunity/JosephcommunityApplication.java
@@ -0,0 +1,14 @@
+package cn.yanshijie.josephcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class JosephcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JosephcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yanshijie/josephcommunity/JosephcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yanshijie.josephcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosephcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
